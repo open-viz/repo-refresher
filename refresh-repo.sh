52522b6102208ae78e07883e15760d49646706c9
Tamal Saha <tamal@appscode.com>
2021-08-16 11:15:34 -0700

    Update kubedb repos
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -37,10 +37,11 @@ refresh() {
             go mod tidy
         fi
         go mod edit \
-            -require=kmodules.xyz/client-go@dd0503cf99cf3b6abb635d8945a8d7d8fed901d9 \
+            -require=kmodules.xyz/client-go@625868881acdceed356e65dc0a211ac81a5d0e4a \
             -require=kmodules.xyz/webhook-runtime@e489faf01981d2f3afa671989388c7b6f22b6baa \
-            -require=kmodules.xyz/resource-metadata@dcc1abc08aa00646b9474f7702b45c798b3ce66c \
-            -require=kmodules.xyz/custom-resources@83db827677cf5651491478fa85707d62416cf679 \
+            -require=kmodules.xyz/resource-metadata@71d20972a852023700c09915cbe6ccab0321ac17 \
+            -require=kmodules.xyz/custom-resources@1631fae03a1aa8ea79f1321f133b1255a71e025e \
+            -require=gomodules.xyz/x@v0.0.7 \
             -replace=github.com/satori/go.uuid=github.com/gofrs/uuid@v4.0.0+incompatible \
             -replace=helm.sh/helm/v3=github.com/kubepack/helm/v3@v3.6.1-0.20210518225915-c3e0ce48dd1b \
             -replace=k8s.io/apiserver=github.com/kmodules/apiserver@v0.21.2-0.20210716212718-83e5493ac170
